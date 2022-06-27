0ad5de3be4197729710e09076441ea629e7add82
dowon <dowon@dowonui-MacBookAir.local>
2022-06-27 22:17:12 +0900

    오류 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { MdDone, MdDelete } from "react-icons/md";
 import { css } from "styled-components";
 import styled from "styled-components";
