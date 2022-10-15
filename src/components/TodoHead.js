f64aacb277a980cc745d0568133df57e8ac45888
dowon <dowon@dowonui-MacBookAir.local>
2022-10-15 16:27:12 +0900

    날짜 오류 수정ㅇ

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -43,7 +43,7 @@ const TodoHead = () => {
 
     return (
         <TodoHeadBlock>
-            <h1>{`${today.getFullYear()}년 ${today.getMonth()}월 ${today.getDate()}일`}</h1>
+            <h1>{`${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`}</h1>
             <div className="day">{`${days[today.getDay()]}`}</div>
             <div className="tasks-left">{`할 일 ${undoneTasks.length}개 남음`}</div>
         </TodoHeadBlock>
